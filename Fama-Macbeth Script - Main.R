17139bab731dc4f919973a456c369fa1a0193165
tyu8044 <tyu8044@naver.com>
2023-10-24 20:27:03 +1000

    Section A to C on Data extraction and Portfolio formation period

--- Fama-Macbeth Script - Main.R
+++ Fama-Macbeth Script - Main.R
@@ -39,3 +39,146 @@ full_data <- crsp_query |>
 
 full_data$year_month <- format(full_data$date, "%Y-%m")
 
+#SECTION A - RISK FREE RATE FROM FAMA FRENCH
+#####################################
+temp <- tempfile(fileext = ".zip")
+download.file("https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_CSV.zip",temp)
+temp1 <- unzip(temp, exdir = ".")
+
+
+
+ff_3factors_monthly <- read.csv(temp1, skip=5, header = F) 
+names(ff_3factors_monthly) <- c('dt', 'rmrf', 'smb', 'hml', 'rf')
+unlink(temp)
+unlink(temp1)
+head(ff_3factors_monthly)
+
+
+
+ff_3factors_mon <- ff_3factors_monthly |> 
+  filter(nchar(dt) == 6) |> 
+  mutate(yr = str_sub(dt,1,4), mon= str_sub(dt,-2,-1),  
+         date = make_date(year= yr, month = mon, day = 01),
+         mkt_excess = as.numeric(rmrf), smb = as.numeric(smb),
+         hml = as.numeric(hml), rf = as.numeric(rf)) |> 
+  select(c('date','mkt_excess','smb','hml','rf'))
+ff_3factors_mon <- as_tibble(ff_3factors_mon)
+
+
+
+ff_3factors_mon$date <- as.Date(ff_3factors_mon$date)
+ff_3factors_mon$date <- ceiling_date(ff_3factors_mon$date, "month") - days(1)
+ff_3factors_mon$year_month <- format(ff_3factors_mon$date, "%Y-%m")
+
+
+
+risk_free<-ff_3factors_mon|>
+  select(rf, year_month)|>
+  mutate(rf=rf/100)
+
+
+
+#MERGE
+full_data_with_rf <- full_data |> 
+  left_join(
+    risk_free, 
+    by = c('year_month'), keep=FALSE)
+########################################################
+
+
+
+#SECTION B: Set up dataset in local DB
+
+
+
+setwd("C:/Users/gkimu/OneDrive/Documents/GitHub/Fama-Macbeth-Group-Work")
+Fama_Macbeth<- dbConnect(
+  SQLite(),
+  "data/FM_data.sqlite",
+  extended_types = TRUE
+)
+
+
+
+dbWriteTable(Fama_Macbeth,
+             "full_data", #name of table
+             value = full_data, #content of table
+             overwrite = TRUE )
+
+
+
+dbListTables(Fama_Macbeth)
+
+
+
+
+
+#SECTION C - Extract Data for each of the Periods
+#SECTION C1 - Portfolio Formation  to Testing Period
+period2 <- tbl(Fama_Macbeth, "full_data")|> 
+  select(permno, date, ret, primexch, shrcd) |> 
+  collect()|>
+  mutate(date=as.Date(date, format="%Y-%m-%d"))|>
+  mutate(month = floor_date(date, "month"), year=year(date))|>
+  na.omit()|>
+  group_by(month)|>
+  mutate(mkt = mean(ret))|>
+  filter(date>='1927-01-01' & date <= '1942-12-31')|>
+  ungroup()
+
+
+
+
+
+count1<- period2 %>% group_by(permno) %>%  #no of securities available 
+  summarise(total_count=n(),
+            .groups = 'drop')
+
+
+
+#SECTION C2 - Portfolio Formation Period Data
+crsp_pf_period2 <- tbl(Fama_Macbeth, "full_data")|> 
+  select(permno, date, ret, primexch) |> 
+  collect()|>
+  mutate(date=ymd(date))|>
+  mutate(month = floor_date(date, "month"), year=floor_date(date, "year"))|>
+  na.omit()|>
+  group_by(month)|>
+  mutate(mkt = mean(ret))|>
+  filter(date>='1927-01-01' & date<='1933-12-31')
+
+
+
+
+
+#Create beta estimation function
+estimate_capm <- function(data, min_obs) {
+  if (nrow(data) < min_obs) {
+    beta <- as.numeric(NA)
+    sdres <- as.numeric(NA)
+  } else {
+    fit <- lm(ret~mkt, data = data)
+    beta <- as.numeric(coefficients(fit)[2])
+    sdres <- as.numeric(sd(residuals(fit), na.rm=TRUE))
+  }
+  return(tibble(
+    beta,
+    sdres))
+}
+
+
+
+#SECTION C2.2 - Estimation of Pre-Ranking Betas - Portfolio Formation Period
+#Note: Minimum number of observation from period 2 onwards is now 4 years
+beta_pf_period2 <- crsp_pf_period2 |>
+  group_by(permno) |>
+  mutate(pf=estimate_capm(pick(everything()), min_obs = 48)) |>##picks everything from last pipe and uses that as the dataset
+  ungroup()|>
+  drop_na()|>
+  select(permno, pf) |>
+  unique()|>
+  arrange(pf$beta)|>
+  mutate(port=as.numeric(cut_number(pf$beta,20)))|> 
+  drop_na()
+#The last line on "mutate" Uses "cut_number" to sort the betas into 20 portfolios, after sorting in ascending order
+
